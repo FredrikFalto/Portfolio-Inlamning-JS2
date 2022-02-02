ca818c3d3c18fd2edc16150dc5123ef64a82a286
FredrikFalto <faltofredrik@gmail.com>
2022-02-02 11:26:49 +0100

    Added email functionality to the form on the contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,70 @@
-import React from "react";
+import React, { useRef } from "react";
 import Footer from "./Footer";
+import emailjs from "emailjs-com";
 
-function Contact() {
+export default function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_hpu21mk",
+                "template_24g00ht",
+                form.current,
+                "user_ozHdLSjLhRnSStgtGJyqr"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+        e.target.reset();
+    };
+
     return (
         <section>
             <div className="container">
                 <div className="grid">
                     <div className="col-12">
                         <div className="contactForm">
-                            <form autoComplete="off" action="">
+                            <form autoComplete="off" ref={form} onSubmit={sendEmail}>
                                 <h1>Contact me 🙂</h1>
 
                                 <ul>
                                     <li>
-                                        <input type="text" placeholder="Name" />
+                                        <input
+                                            type="text"
+                                            placeholder="Name"
+                                            name="name"
+                                            id="nameTxt"
+                                        />
+                                    </li>
+                                    <li>
+                                        <input
+                                            type="text"
+                                            placeholder="Subject"
+                                            name="subject"
+                                            id="subjectTxt"
+                                        />
                                     </li>
                                     <li>
                                         <input
                                             type="text"
                                             placeholder="Email"
+                                            name="email"
+                                            id="emailTxt"
                                         />
                                     </li>
                                     <li>
                                         <textarea
                                             rows="6"
                                             placeholder="Message"
+                                            name="message"
                                         ></textarea>
                                     </li>
                                     <li>
@@ -43,5 +83,3 @@ function Contact() {
         </section>
     );
 }
-
-export default Contact;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,25 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="social">
-                <a href="https://github.com/FredrikFalto">
+                <a
+                    href="https://github.com/FredrikFalto"
+                    target={"_blank"}
+                    rel="noreferrer"
+                >
                     <FaGithub className="social-icon" />
                 </a>
-                <a href="https://www.linkedin.com/in/fredrik-f%C3%A4lt%C3%B6-a54b97113/">
+                <a
+                    href="https://www.linkedin.com/in/fredrik-f%C3%A4lt%C3%B6-a54b97113/"
+                    target={"_blank"}
+                    rel="noreferrer"
+                >
                     <FaLinkedin className="social-icon" />
                 </a>
-                <a href="https://www.facebook.com/fredrik.falto/">
+                <a
+                    href="https://www.facebook.com/fredrik.falto/"
+                    target={"_blank"}
+                    rel="noreferrer"
+                >
                     <FaFacebook className="social-icon" />
                 </a>
             </div>
